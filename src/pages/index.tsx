026f909c0ee2e8dd72caeb5f2694b52d6e231815
Eltik <xp10d363@gmail.com>
2023-06-12 10:21:52 -0700

    Footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -269,6 +269,59 @@ const Home: NextPage = () => {
                         </div>
                     </div>
                 </div>
+                <footer className="bg-gray-100 py-8">
+                    <div className="container mx-auto px-4">
+                        <div className="flex flex-wrap">
+                            <div className="w-full lg:w-1/4 mb-8">
+                                <h2 className="text-xl font-bold">Contact</h2>
+                                <p className="text-gray-500 mt-4">Discord: imeltik</p>
+                                <p className="text-gray-500 mt-4">Email: eltik@eltik.net</p>
+                                <p className="text-gray-500">Phone: (123)-456-7890</p>
+                            </div>
+                            <div className="w-full lg:w-1/4 mb-8">
+                                <h2 className="text-xl font-bold">Links</h2>
+                                <ul className="mt-4">
+                                    <li><Link href="#" className="text-gray-500 hover:text-gray-400 transition duration-150">Home</Link></li>
+                                    <li><Link href="/projects" className="text-gray-500 hover:text-gray-400 transition duration-150">Projects</Link></li>
+                                    <li><Link href="/youtube" className="text-gray-500 hover:text-gray-400 transition duration-150">YouTube</Link></li>
+                                    <li><Link href="/premium" className="text-gray-500 hover:text-gray-400 transition duration-150">Premium+</Link></li>
+                                </ul>
+                            </div>
+                            <div className="w-full lg:w-1/4 mb-8">
+                                <h2 className="text-xl font-bold">Social Media</h2>
+                                <div className="flex flex-row gap-2">
+                                    <Link href="#" className="text-gray-500 hover:text-gray-400 transition duration-300">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="">
+                                            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/>
+                                        </svg>
+                                    </Link>
+                                    <Link href="#" className="text-gray-500 hover:text-gray-400 transition duration-300">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="">
+                                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/>
+                                        </svg>
+                                    </Link>
+                                    <Link href="#" className="text-gray-500 hover:text-gray-400 transition duration-300">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="">
+                                            <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"/>
+                                        </svg>
+                                    </Link>
+                                    <Link href="#" className="text-gray-500 hover:text-gray-400 transition duration-300">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="">
+                                            <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"/><path d="m10 15 5-3-5-3z"/>
+                                        </svg>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className="w-full lg:w-1/4 mb-8">
+                                <h2 className="text-xl font-bold">Coming soon!</h2>
+                                <p className="text-gray-500 mt-4">More details coming soon!</p>
+                            </div>
+                        </div>
+                        <div className="border-t border-gray-700 mt-8 pt-4">
+                        <p className="text-gray-400 text-sm text-center">© 2023 Eltik. All rights reserved.</p>
+                        </div>
+                    </div>
+                </footer>
             </main>
         </>
     );
